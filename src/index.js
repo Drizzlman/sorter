056af800d2f6ef3be23b123339ddca928d8f9a9a
Drizzlman <kovalko.alexey@yandex.by>
2018-09-21 21:01:41 +0300

    Task_3

--- src/index.js
+++ src/index.js
@@ -1,31 +1,42 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.Array = [];
+        this.Compare = (left, right) => left - right;
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this.Array.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.Array[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this.Array.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this.Array;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+        var SortedArray = [];
+        var SortedInd = indices.sort((left, right) => left - right);
+        for (var i = 0, length = SortedInd.length; i < length; i++) {
+            SortedArray.push(this.Array[SortedInd[i]]);
+        }
+        SortedArray.sort(this.Compare);
+        for (var i = 0, length = SortedInd.length; i < length; i++) {
+            this.Array[SortedInd[i]] = SortedArray.shift();
+        }
+    }
+
+
+    setComparator(compareFunction) {
+        this.Compare = compareFunction;
+    }
 }
 
 module.exports = Sorter;
